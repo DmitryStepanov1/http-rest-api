d09531c56bb3e7a59cc8f28be1b362cbd6494636
DmitryStepanov1 <ds.dstepanov@gmail.com>
2022-11-25 19:32:18 +0300

    validation added

--- internal/app/store/userrepository.go
+++ internal/app/store/userrepository.go
@@ -7,7 +7,16 @@ type UserRepository struct {
 	store *Store
 }
 
+// Create ...
 func (r *UserRepository) Create(u *model.User) (*model.User, error) {
+	if err := u.Validate(); err != nil {
+		return nil, err
+	}
+
+	if err := u.BeforeCreate(); err != nil {
+		return nil, err
+	}
+
 	if err := r.store.db.QueryRow(
 		"INSERT INTO users (email, encrypted_password) VALUES ($1, $2) RETURNING id",
 		u.Email,
@@ -19,6 +28,19 @@ func (r *UserRepository) Create(u *model.User) (*model.User, error) {
 	return u, nil
 }
 
-func (r *UserRepository) FindByEmail(email string) (model.User, error) {
-	return nil, nil
+// FindByEmail ...
+func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
+	u := &model.User{}
+	if err := r.store.db.QueryRow(
+		"SELECT id, email, encrypted_password FROM users WHERE email = $1",
+		email,
+	).Scan(
+		&u.ID,
+		&u.Email,
+		&u.EncryptedPassword,
+	); err != nil {
+		return nil, err
+	}
+
+	return u, nil
 }
